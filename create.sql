32f9a6ef1399ce0acc71547fcb69d81f62e51581
TaniaZakowski <tania.zakowski@gmail.com>
2024-08-27 16:19:36 +0200

    lab solved

--- create.sql
+++ create.sql
@@ -2,45 +2,54 @@
 	
 	USE lab_mysql;
     
-    DROP TABLE IF EXISTS customers;
-	CREATE TABLE customers (
-    `customer_id` INT NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NULL,
-    `phone_number` VARCHAR(255) NULL,
-    `adress` VARCHAR(255) NOT NULL,
-    `city` VARCHAR(255) NOT NULL,
-    `zipcode` INT NOT NULL,
-    `state_province` TEXT NOT NULL, 
-    `country` TEXT NOT NULL,
-    `ref_customer` INT NOT NULL AUTO_INCREMENT PRIMARY KEY
-	);
+
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS customers;    
+CREATE TABLE customers (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    cust_id INT NOT NULL,
+    cust_name VARCHAR(50) NOT NULL,
+    cust_phone VARCHAR(20),
+    cust_email VARCHAR(50),
+    cust_address  VARCHAR(100) NOT NULL,
+    cust_city  VARCHAR(100) NOT NULL,
+    cust_state_province  VARCHAR(100) NOT NULL,
+    cust_zipcode INT  NOT NULL,
+    cust_country VARCHAR(100) NOT NULL,
+    CONSTRAINT unique_cust_id UNIQUE (cust_id)
+);
 
 	DROP TABLE IF EXISTS salepersons;
 	CREATE TABLE salepersons(
-    `staff_id` VARCHAR(255) NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    `updated_at` DATETIME NOT NULL,
-    `ref_staff` INT NOT NULL AUTO_INCREMENT PRIMARY KEY 
+    staff_id VARCHAR(255) NOT NULL,
+    staff_name VARCHAR(255) NOT NULL,
+    staff_start_date DATE NOT NULL,
+    ref_staff INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    CONSTRAINT  unique_staff_id UNIQUE (staff_id)
 );
     
 	DROP TABLE IF EXISTS cars;
 	CREATE TABLE cars (
-	`vin` CHAR(255) NOT NULL ,
-    `manufacturer` VARCHAR(255) NULL,
-    `model` VARCHAR(255) NULL,
-    `year` CHAR(4) NOT NULL,
-    `color` TEXT NOT NULL,
-    `ref_car` INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+	car_vin CHAR(255) NOT NULL ,
+    car_manufacturer VARCHAR(255) NULL,
+    car_model VARCHAR(255) NULL,
+    car_year CHAR(4) NOT NULL,
+    car_color TEXT NOT NULL,
+    ref_car INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    CONSTRAINT unique_car_vin UNIQUE (car_vin)
 	);
 
-	DROP TABLE IF EXISTS invoices;
+DROP TABLE IF EXISTS invoices;
 CREATE TABLE invoices (
-    `sale_person` INT NOT NULL,
-    `id_sale` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `sale_date` DATE NOT NULL,
-    `price` FLOAT NOT NULL,
-    `vehicle` CHAR(255) NOT NULL
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    invoice_number VARCHAR(20) NOT NULL,
+    inv_date DATE NOT NULL,
+    car_id CHAR(255)  NOT NULL,
+    customer_id INT  NOT NULL,
+    salesperson_id VARCHAR(255)  NOT NULL,
+    CONSTRAINT fk_car_id FOREIGN KEY (car_id) REFERENCES cars(car_vin),
+    CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customers(cust_id),
+    CONSTRAINT fk_salesperson_id FOREIGN KEY (salesperson_id) REFERENCES salepersons(staff_id),
+    CONSTRAINT unique_invoice_number UNIQUE (invoice_number)
 );
-  
     
--- seeding.sql
+++ seeding.sql
@@ -1,17 +1,17 @@
 USE lab_mysql;
 
-INSERT INTO customers (customer_id,name,adress,city,zipcode,state_province,country)
+INSERT INTO customers (cust_id,cust_name,cust_address,cust_city,cust_zipcode,cust_state_province,cust_country)
 VALUES (0, 'Pablo Picasso','8 rue des Lilas','Montbonot','78643','France','France'),
 	   (2, 'Pabla Picasso','12 rue des Anemones','Metz','57000','France','France');
        
-INSERT INTO cars (vin,year,color)
+INSERT INTO cars (car_vin,car_year,car_color)
 VALUES ('ZM8G7BEUQZ97IH46V','2020','blue'),
 	   ('DAM41UDN3CHU2WVF6','2023','red');
 
-INSERT INTO salepersons (staff_id,name,starting_date)
-VALUES ('JMLEPLOTE','Jean-Marie Leplote',2018-02-08),
-       ('PMOLIVE','Pierre Molive',2021-12-12);
+INSERT INTO salepersons (staff_id,staff_name,staff_start_date)
+VALUES ('JMLEPLOTE','Jean-Marie Leplote',20180208),
+       ('PMOLIVE','Pierre Molive',20211212);
        
-INSERT INTO invoices (sale_person,sale_date,price,vehicule)
-VALUES ('JMLEPLOTE',2024-07-08,45632,'ZM8G7BEUQZ97IH46V'),
-	   ('JMLEPLOTE',2024-01-06,18750,'DAM41UDN3CHU2WVF6');
+INSERT INTO invoices (salesperson_id,inv_date,car_id,customer_id)
+VALUES ('JMLEPLOTE',20240708,45632,'ZM8G7BEUQZ97IH46V',O),
+	   ('JMLEPLOTE',20240106,18750,'DAM41UDN3CHU2WVF6',2);
